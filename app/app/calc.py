60e6fef319fc0c7715f5728d0e35a7e07fe88f0e
jhonnelrem <jhonnelrem@gmail.com>
2025-06-24 00:04:42 +0800

    Fix error v1

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,10 @@
 Calculator function
 """
 
-def add(x,y):
+def add(x, y):
     """Add x and y and return the results."""
     return x + y
 
-def subtract(x,y):
+def subtract(x, y):
     """Subtract x and y and return the results."""
     return x - y
