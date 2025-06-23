12c111ccbef1aa0488f72881da440b8f55fc1e3d
jhonnelrem <jhonnelrem@gmail.com>
2025-06-24 00:01:38 +0800

    FIx

--- app/app/tests.py
+++ app/app/tests.py
@@ -8,16 +8,13 @@ from app import calc
 
 class CalcTest(SimpleTestCase):
     """Test the calc module."""
-
     def test_add_numbers(self):
         """Test adding numbers together."""
         res = calc.add(5,6)
-        
         self.assertEqual(res,11)
 
     def test_subtract_numbers(self):
         """Test adding numbers together."""
         res = calc.subtract(15,10)
-        
         self.assertEqual(res,5)
 
